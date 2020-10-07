4c7b05fdabb3699f98cd32acf84c1bd478a4f1e8
Sketch-Plane <kjx765567@163.com>
2020-10-08 01:19:02 +0800

    完成了侧边栏数据获取和渲染

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,22 +1,99 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+    <el-container class="home-container">
+      <!-- 头部区域 -->
+      <el-header>
+        <div>
+          <img src="../assets/heima.png" alt="">
+          <span>电商后台管理系统</span>
+        </div>
+        <el-button type="info" @click="logout">退出</el-button>
+      </el-header>
+      <!-- 页面主题区域 -->
+      <el-container>
+          <!-- 侧边栏 -->
+        <el-aside width="200px">
+          <!-- 侧边栏菜单区域 -->
+          <el-menu background-color="#333744" text-color="#fff" active-text-color="#ffd04b">
+            <!-- 一级菜单 -->
+            <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+              <!-- 一级菜单模板区域 -->
+              <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>{{item.authName}}</span>
+              </template>
+              <!-- 二级菜单 -->
+              <el-menu-item :index="subItem.id + ''" v-for="subItem in item.children" :key="subItem.id">
+                <template slot="title">
+                <!-- 图标 -->
+                <i class="el-icon-location"></i>
+                <!-- 文本 -->
+                <span>{{subItem.authName}}</span>
+                </template>
+              </el-menu-item>
+            </el-submenu>
+          </el-menu>
+        </el-aside>
+          <!-- 右侧内容主体 -->
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
 </template>
 
 <script>
 export default {
+  data()  {
+    return {
+      //左侧菜单数据
+      menuList: []
+    }
+    
+  },
+  created() {
+    this.getMenuList()
+  },
   methods: {
     logout() {
       //清空token
       window.sessionStorage.clear()
       //跳转到登录页
       this.$router.push('/login')
+    },
+    async getMenuList() {
+      const {data: res} = await this.$http.get('menus')
+      if(res.meta.status !== 200) return this.$message.error(res.meta.msg);
+      this.menuList = res.data
+      // console.log(res);
     }
   }
 }
 </script>
 
-<style>
-
+<style scoped>
+.home-container {
+  height: 100%;
+}
+.el-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding-left: 0;
+  background-color: #373d41;
+  color: #fff;
+  font-size: 20px;
+}
+.el-header div {
+  display: flex;
+  align-items: center;
+}
+.el-header div span {
+  margin-left: 15px;
+}
+.el-aside {
+  background-color: #333744;
+}
+.el-main {
+  background-color: #eaedf1;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -11,6 +11,13 @@ Vue.use(ElementUI);
 import axios from "axios"
 //配置请求根路径
 axios.defaults.baseURL = "http://127.0.0.1:8888/api/private/v1/"
+//请求拦截器
+axios.interceptors.request.use((config) => { 
+  // console.log(config);
+  //为请求头对象添加token验证的Authorization字符段
+  config.headers.Authorization = window.sessionStorage.getItem('token')
+  return config
+})
 //将axios挂载在Vue原型上的$http上，这样每个组件都可以通过$http发送网络请求
 Vue.prototype.$http = axios
 Vue.prototype.$message = Message
